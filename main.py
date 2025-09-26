991b4eb0c83a00c77b53ae2a4d3cc9da122abf83
Platinum-Pluto <mediocre.judge2000@gmail.com>
2025-09-27 00:39:35 +0600

    .

--- main.py
+++ main.py
@@ -18,16 +18,14 @@ CHUNK_SIZE = 2000
 
 
 load_dotenv()
-hf = os.getenv("HF")
-openai_api = os.getenv('OPENAI')
-mistral = os.getenv('MISTRAL')
-
-
-claude = os.getenv('CLAUDE')
-grok = os.getenv('GROK')
-gemini = os.getenv('GEMINI')
-open_router = os.getenv('OPEN_ROUTER')
-BASE_URL = os.getenv("BASE_URL")
+hf = os.environ["HF"]
+openai_api = os.getenv('OPENAI') or None
+mistral = os.getenv('MISTRAL') or None
+claude = os.getenv('CLAUDE') or None
+grok = os.getenv('GROK') or None
+gemini = os.getenv('GEMINI') or None
+open_router = os.getenv('OPEN_ROUTER') or None
+BASE_URL = os.environ["BASE_URL"] 
 
 if not hf or not BASE_URL:
     raise RuntimeError("❌ Missing HF or BASE_URL environment variable. Check GitHub secrets.")
